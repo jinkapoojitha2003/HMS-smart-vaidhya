9d3555bee5c599db498276173558b5bcec12d67d
jinkapoojitha2003 <125655960+jinkapoojitha2003@users.noreply.github.com>
2024-11-30 14:48:05 +0530

    Update emr_access.php

--- emr_access.php
+++ emr_access.php
@@ -2,7 +2,7 @@
 // Database connection
 $servername = "localhost:3306";
 $username = "root";
-$password = "Poojitha";
+$password = "";
 $dbname = "hospital_management";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
